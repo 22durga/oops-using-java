73122b87b6ba51498cbb2a25f107cd5c754b67b1
saidurga <124652987+22durga@users.noreply.github.com>
2023-02-07 10:25:15 +0530

    addition of two files
    add 2 integer number

---
+++ Addint.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class Addint{
+public static void main (String args[])
+{
+Scanner s=new Scanner (System.in);
+System.out.println("Enter the number:");
+int a=s.nextInt();
+int b=s.nextInt();
+System.out.println(a+b);
+}}
