568375f481ad5e8f0b7991d5cefeac838482e657
saidurga <124652987+22durga@users.noreply.github.com>
2023-03-05 20:42:18 +0530

    String to bool
    Convert string to boolean

---
+++ demo4.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.Arrays;
+public class demo4{
+public static void main(String[] args){
+String str = "true";
+boolean bool = Boolean.parseBoolean(str);
+System.out.println(bool);
+}
+}
