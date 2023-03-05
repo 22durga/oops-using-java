4720d6bd5f2333ea513a7601bca1784d3659ee32
saidurga <124652987+22durga@users.noreply.github.com>
2023-03-05 21:07:10 +0530

    Find diagonal in matrix
    print the diagonal in of matrix

---
+++ Diagonal.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Diagonal
+{
+public static void main(String args[])
+{
+int n,m;
+int i;
+int j;
+int flag=0;
+int sum=0;
+System.out.println("no of rows of arr");
+Scanner s = new Scanner(System.in);
+n=s.nextInt();
+System.out.println("no of col array ");
+m=s.nextInt();
+int a[][]=new int[n][m];
+int b[][]=new int[n][m];
+System.out.println("enter the elements of array1 are");
+for(i=0;i<n;i++)
+{
+for(j=0;j<m;j++)
+{
+a[i][j]=s.nextInt();
+}
+}
+System.out.println(" the elements of array 1are");
+for(i=0;i<n;i++)
+{
+for(j=0;j<m;j++)
+{
+System.out.print(a[i][j]+" ");
+}
+System.out.println(" ");
+}
+System.out.println("the diagonal elements are");
+for(i=0;i<n;i++)
+{
+	sum =sum+a[i][i];
+}
+System.out.println(sum);
+}}
+
