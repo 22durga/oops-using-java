0c98876d4cc9dd4a90e8c31d82835f9ec3e50fb7
saidurga <124652987+22durga@users.noreply.github.com>
2023-03-05 16:09:23 +0530

    Unary operator
    Example of unary operator

---
+++ Operator.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class Operator{
+public static void main(String args[]){
+int x=10;
+System.out.println(x++);
+System.out.println(++x);
+System.out.println(x--);
+System.out.println(--x);
+}}
