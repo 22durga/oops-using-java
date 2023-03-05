491de678b0c31fc577cd6e2163dd24aad87a7385
saidurga <124652987+22durga@users.noreply.github.com>
2023-03-05 20:49:15 +0530

    print 2d array without loop
    print 2d array without loop

---
+++ demo11.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.Arrays;
+public class demo11{
+public static void main(String[] args){
+int [] [] a={ {2,5,46} , {12,34} };
+System.out.println(Arrays.deepToString(a) );
+}
+}
