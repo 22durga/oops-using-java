2a790efe66dd2421435e52ba8717397e05a35573
saidurga <124652987+22durga@users.noreply.github.com>
2023-03-05 15:39:00 +0530

    Add files via upload
    Getting number from user

---
+++ Firstnum.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+class Firstnum{
+public static void main (String args[])
+{
+Scanner s=new Scanner (System.in);
+System.out.println("Enter the first number:");
+int x=s.nextInt();
+System.out.println(x);
+}}
