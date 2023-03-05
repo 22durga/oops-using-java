39e092a12619bc7f31688a24087a0e2459c1b650
saidurga <124652987+22durga@users.noreply.github.com>
2023-03-05 20:45:38 +0530

    bool to int
    Convert boolean to integer

---
+++ demo7.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.Arrays;
+public class demo7{
+public static void main(String[] args){
+Boolean bool = true;
+System.out.println("Boolean Value: " + bool);
+int val = (bool) ? 1 : 0 ;
+System.out.println("Integer value: " + val);
+}
+}
