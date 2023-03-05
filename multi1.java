af35c3a840b00ef0962c7b76da0972627084e18b
saidurga <124652987+22durga@users.noreply.github.com>
2023-03-05 20:59:45 +0530

    print 2d arr using foreach loop
    print 2d array using foreach loop

---
+++ multi1.java
@@ -0,0 +1,11 @@
+class multi1{
+public static void main(String[] args){
+int a[] [] ={{1,2,3},{-4,-5,6,7},{7,8}};
+for(int [] i : a){
+System.out.println();
+for(int j : i){
+System.out.print(j + " " );
+}
+}
+}
+}
