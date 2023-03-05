45f438c68a7ab31ec6837fc3f72f2b253e4f9d10
saidurga <124652987+22durga@users.noreply.github.com>
2023-03-05 20:57:56 +0530

    Identify length each row in 2d array
    Identify length each row in 2d array

---
+++ multi.java
@@ -0,0 +1,7 @@
+class multi{
+public static void main(String[] args){
+int a [] [] ={{1,2,3},{4,5,6,7},{7}};
+System.out.println("Length of row 1:" + a[0].length);
+System.out.println("Length of row 1:" + a[1].length);
+System.out.println("Length of row 1:" + a[2].length);
+}}
