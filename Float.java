e571b69bae4d60980ae0d09856fe7014f76f9a25
saidurga <124652987+22durga@users.noreply.github.com>
2023-03-05 15:40:35 +0530

    Add files via upload

---
+++ Float.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+class Float{
+public static void main (String args[])
+{
+Scanner s=new Scanner (System.in);
+System.out.println("Enter the number:");
+float a=s.nextFloat();
+System.out.println(a);
+}}
