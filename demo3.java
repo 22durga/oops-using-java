da3d315ad2e919d3057dee4fab9ed38b41062ce1
saidurga <124652987+22durga@users.noreply.github.com>
2023-03-05 20:41:32 +0530

    int to bool
    Convert integer to boolean value

---
+++ demo3.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.Arrays;
+public class demo3{
+public static void main(String[] args){
+int val=100;
+System.out.println("Integer: " + val);
+Boolean bool=(val == 100);
+System.out.println("Converted to bool: " + bool);
+}
+}
+
