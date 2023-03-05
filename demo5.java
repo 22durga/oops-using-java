f0279e770ef533805f7d5861b05139df04124a9f
saidurga <124652987+22durga@users.noreply.github.com>
2023-03-05 20:43:22 +0530

    bool primitive to bool object
    Convert boolean primitive to boolean object

---
+++ demo5.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.Arrays;
+public class demo5{
+public static void main(String[] args){
+boolean val=false;
+Boolean res = Boolean.valueOf(val);
+System.out.println(res);
+}
+}
