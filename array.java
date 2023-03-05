56ac5c1f617c5a7af5ffbba21d3d3317847b013f
saidurga <124652987+22durga@users.noreply.github.com>
2023-03-05 20:32:46 +0530

    Print the array without loop
    Print the array without loop using java

---
+++ array.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.Arrays;
+public class array {
+public static void main(String[] args){
+int a[]={1,2,3,4,5};
+int n;
+n=a.length;
+System.out.println(Arrays.toString(a));
+}}
