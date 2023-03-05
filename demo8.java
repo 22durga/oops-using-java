910c6b2bc76d084abd50c3525b1d43a8dfedc997
saidurga <124652987+22durga@users.noreply.github.com>
2023-03-05 20:46:28 +0530

    int to string
    Convert integer to string

---
+++ demo8.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.Arrays;
+public class demo8{
+public static void main(String[] args){
+int val = 150;
+String str= Integer.toString(val);
+System.out.println("String: " +str);
+}
+}
