682c03462125648581abe06ec724505c9756c235
saidurga <124652987+22durga@users.noreply.github.com>
2023-03-05 16:16:09 +0530

    String using java
    Get  String from user

---
+++ String.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+class String{
+public static void main (String args[])
+{
+Scanner s=new Scanner (System.in);
+System.out.println("Enter a word:");
+String a=s.nextLine();
+System.out.println(a);
+}}
