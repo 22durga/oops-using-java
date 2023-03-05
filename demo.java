10a32b6bcd6f0d92256904e845796e6433d155cd
saidurga <124652987+22durga@users.noreply.github.com>
2023-03-05 20:36:24 +0530

    Array equals
    Compare of two arrays

---
+++ demo.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Arrays;
+public class demo{
+public static void main(String[] args){
+int a []={1,2,3,4};
+int b []={1,2,3,4};
+int c []={1,2,3,4};
+
+System.out.print( Arrays.equals(a,b));
+System.out.print( Arrays.equals(a,c));
+}
+}
