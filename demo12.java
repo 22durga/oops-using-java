fa0f304fd868262c226472d5773b780f78a1528c
saidurga <124652987+22durga@users.noreply.github.com>
2023-03-05 20:50:15 +0530

    Assignment operator
    Example of Assignment operator

---
+++ demo12.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.Arrays;
+public class demo12{
+public static void main(String[] args){
+ int a[]={1,2,3,4,5,6};
+int b[]=a;
+for (int i:b){
+System.out.print(i + ",");
+}
+}
+}
