48c552604668e320721329d5dbf113916ea5363b
saidurga <124652987+22durga@users.noreply.github.com>
2023-03-05 20:55:17 +0530

    Jagged array
    Jagged array using java

---
+++ jagged.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class jagged{
+public static void main(String[] args){
+int a[][]={{10,20,30},{4,5},{6,7,8,9}};
+for(int i=0;i<3;i++)
+{
+for(int j=0;j<a[i].length;j++){
+System.out.print(a[i][j]+" ");
+}
+System.out.println();
+}
+}}
+
