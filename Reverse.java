fb40d008f7c37488f22566d5a9dde08351e126ab
saidurga <124652987+22durga@users.noreply.github.com>
2023-03-05 20:13:57 +0530

    Reverse the number
    Print reverse of number in java using java

---
+++ Reverse.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class Reverse
+{
+    public static void main (String args[])
+    {
+        int i;
+        for ( i=10 ; i>=1 ; i-- )
+        {
+            System.out.println(10 + "*" + i + "=" + i*10);
+        }
+
+    }      
+}
